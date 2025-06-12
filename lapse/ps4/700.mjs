72a7d3376072bdaac1b4376fc9eecd4866438f6b
Benny <164119563+boredmonkeyman@users.noreply.github.com>
2025-06-12 14:55:17 -0700

    Update 700.mjs

--- lapse/ps4/700.mjs
+++ lapse/ps4/700.mjs
@@ -1,21 +1,3 @@
-/* Copyright (C) 2025 anonymous
-
-This file is part of PSFree.
-
-PSFree is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
-
-PSFree is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
-
-// 7.00, 7.01, 7.02
 
 export const pthread_offsets = new Map(
   Object.entries({
