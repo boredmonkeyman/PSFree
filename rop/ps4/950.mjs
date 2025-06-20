058be3c89d781c4a65473dd07bb008244540d8b7
Benny <164119563+boredmonkeyman@users.noreply.github.com>
2025-06-20 21:18:05 +0100

    Update 950.mjs

--- rop/ps4/950.mjs
+++ rop/ps4/950.mjs
@@ -1,23 +1,3 @@
-/* Copyright (C) 2023-2025 anonymous
-
-This file is part of PSFree.
-
-PSFree is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
-
-PSFree is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
-
-// 9.50, 9.51, 9.60
-// ROP Chain by @janisslsm
-
 import { mem } from "../../module/mem.mjs";
 import { KB } from "../../module/offset.mjs";
 import { ChainBase } from "../../module/chain.mjs";
